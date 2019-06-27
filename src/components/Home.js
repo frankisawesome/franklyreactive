3a01966cb03eeeb1fd6db6b658ff2feef0076951
Frank Li <b439988l@gmail.com>
2019-06-27 12:59:29 +0800

    stewie section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Jumbo } from './Jumbo';
 import { Showcase } from './Showcase';
-import myhead from '../images/myhead.jpg'
+import myhead from '../images/myhead.jpg';
+import stewie from '../images/stewie.jpg';
+
+
 export const Home = () => {
     
     //Set of props that can be easily modified to populate the components
@@ -18,11 +21,18 @@ export const Home = () => {
         attributes: ["Vice-Chancellor's Scholar at QUT", "CS/Stats - GPA 6.5", "React-Express-Node-Mongo Stack", "Strong stats background", "Interest in Machine Learning/AI"]
     }
 
+    const stewieProps = {
+        image: stewie,
+        heading: "Stewie the cat",
+        attributes: ["Adopted by Frank at the age of 6 weeks", "Pooped on the floor only once", "Will bite your ass off", "Still an absolute cutie", `Has his own page on my website`]
+    }
+
     //Render
     return(
         <div>
             <Showcase {...showcaseProps}/>
             <Jumbo {...jumboProps}/>
+            <Showcase {...stewieProps} />
         </div>
     )
 }
