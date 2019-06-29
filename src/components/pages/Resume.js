5ea67293ab8ee5ac6bd2af471e54c1b886d0f2ee
Frank Li <b439988l@gmail.com>
2019-06-29 10:45:39 +0800

    before service worker

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from "react-pdf";
 import file from '../../images/Resume.pdf'
 
 export const Resume = (props) => {
     return (
         <div>
             <Document file={file}>
-                <Page pageNumber={1} />
+                <Page pageNumber={1} width='600px'/>
             </Document>
         </div>
     )
