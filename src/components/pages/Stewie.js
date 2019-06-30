abdb654543341753e16991aea696cfcdd9e7a926
Frank Li <b439988l@gmail.com>
2019-06-30 22:58:11 +0800

    stewie hero add

--- src/components/pages/Stewie.js
+++ src/components/pages/Stewie.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Gallery from 'react-grid-gallery';
 import '../../css/gallery.css'
+
+import img from '../../images/stewiebig.jpg'
+
+import LazyHero from 'react-lazy-hero';
 
 
 export const Stewie = () => {
     return (
         <div>
-                <h3>Stewie the cat</h3>
+                <LazyHero imageSrc={img}
+                opacity={0.4}
+                className="hero"
+                color='#A7886A'>
+                    <h1>Stewie the cat.</h1>
+            </LazyHero>
                 <Gallery images={images}/>
         </div>
     )
