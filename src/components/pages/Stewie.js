54385b042f877d02467247600f00929742f32921
Frank Li <b439988l@gmail.com>
2019-06-30 13:42:00 +0800

    small fixes b4 deploy

--- src/components/pages/Stewie.js
+++ src/components/pages/Stewie.js
@@ -1,52 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Gallery from 'react-grid-gallery';
 import '../../css/gallery.css'
 
 
-export const Stewie = (props) => {
-    const images = [
-        {
-            src: 'https://i.imgur.com/8vkcKhU.jpg',
-            thumbnail: 'https://i.imgur.com/8vkcKhU.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/VmVYrPJ.jpg',
-            thumbnail: 'https://i.imgur.com/VmVYrPJ.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/sJvPWiy.jpg',
-            thumbnail: 'https://i.imgur.com/sJvPWiy.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/rKmEGqP.jpg',
-            thumbnail: 'https://i.imgur.com/rKmEGqP.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/p2vXWYc.jpg',
-            thumbnail: 'https://i.imgur.com/p2vXWYc.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/1hFnYBr.jpg',
-            thumbnail: 'https://i.imgur.com/1hFnYBr.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/RlUDJ1d.jpg',
-            thumbnail: 'https://i.imgur.com/RlUDJ1d.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/09VVulg.jpg',
-            thumbnail: 'https://i.imgur.com/09VVulg.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/rPyQ4t0.jpg',
-            thumbnail: 'https://i.imgur.com/rPyQ4t0.jpg'
-        },
-        {
-            src: 'https://i.imgur.com/PRCw9fk.jpg',
-            thumbnail: 'https://i.imgur.com/PRCw9fk.jpg'
-        },
-    ]
-
+export const Stewie = () => {
     return (
         <div>
                 <h3>Stewie the cat</h3>
@@ -54,3 +11,46 @@ export const Stewie = (props) => {
         </div>
     )
 }
+
+const images = [
+    {
+        src: 'https://i.imgur.com/8vkcKhU.jpg',
+        thumbnail: 'https://i.imgur.com/8vkcKhU.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/VmVYrPJ.jpg',
+        thumbnail: 'https://i.imgur.com/VmVYrPJ.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/sJvPWiy.jpg',
+        thumbnail: 'https://i.imgur.com/sJvPWiy.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/rKmEGqP.jpg',
+        thumbnail: 'https://i.imgur.com/rKmEGqP.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/p2vXWYc.jpg',
+        thumbnail: 'https://i.imgur.com/p2vXWYc.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/1hFnYBr.jpg',
+        thumbnail: 'https://i.imgur.com/1hFnYBr.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/RlUDJ1d.jpg',
+        thumbnail: 'https://i.imgur.com/RlUDJ1d.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/09VVulg.jpg',
+        thumbnail: 'https://i.imgur.com/09VVulg.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/rPyQ4t0.jpg',
+        thumbnail: 'https://i.imgur.com/rPyQ4t0.jpg'
+    },
+    {
+        src: 'https://i.imgur.com/PRCw9fk.jpg',
+        thumbnail: 'https://i.imgur.com/PRCw9fk.jpg'
+    },
+]
--- src/components/reusables/Hero.js
+++ src/components/reusables/Hero.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 //Internal imports
 import img from '../../images/hero.jpg';
-import '../../css/Hero.css'
+import '../../css/Hero.css';
+import Typing from 'react-typing-animation';
 
 //Libraries
 import LazyHero from 'react-lazy-hero';
 import FormControl from 'react-bootstrap/FormControl'
 
-export const Hero = (props) => {
-    const [message, setMessage] = useState("Write your own inspiration");
+export const Hero = () => {
+    const [message, setMessage] = useState("Be your own inspiration");
+    const [finished, setFinished] = useState(false);
 
     return (
         <LazyHero imageSrc={img}
@@ -17,10 +19,14 @@ export const Hero = (props) => {
             className="hero"
             color="#A7886A"
         >
-            <h1>{message}</h1>
+            {finished ? <h1>{message}</h1> : null}
+            {finished ? null :
+                <Typing onFinishedTyping={() => setFinished(true)} hideCursor={true}>
+                    <h1>{message}</h1>
+                </Typing>}
             <FormControl
-                value = {message}
-                onChange = {(e) => setMessage(e.target.value)}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 className="input"
             />
         </LazyHero>
