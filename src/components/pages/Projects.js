fe554430b8301fcde95ce09f0748e9bcc06eae76
Frank Li <b439988l@gmail.com>
2019-06-30 22:32:07 +0800

    carousels done for project

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import LazyHero from 'react-lazy-hero';
-import img from '../../images/proj.jpg'
-import '../../css/Hero.css'
+
+//Importing my stuff here
+import img from '../../images/proj.jpg';
+import '../../css/Hero.css';
+import { Project } from '../reusables/Project';
+
+import slide1 from '../../images/slide1.png';
+import slide2 from '../../images/slide2.png';
+import slide3 from '../../images/slide3.png';
+import slide4 from '../../images/slide4.png';
+import slide5 from '../../images/slide5.png';
 
 export const Proj = () => {
     return (
@@ -12,6 +21,40 @@ export const Proj = () => {
                 color='#A2AEA3'>
                     <h1>Dream Big.</h1>
             </LazyHero>
+            <Project slides={projProps} />
         </div>
     )
 }
+
+const projProps = [
+    {
+        title: 'Cosmos Point Six',
+        label: 'MERN stack',
+        caption: 'Simple demo of every piece of technology.',
+        src: slide1
+    },
+    {
+        title: 'Cosmos Point Six',
+        label: 'Mono-repo',
+        caption: 'Deployed via Heroku Dyno free tier',
+        src: slide2
+    },
+    {
+        title: 'Cosmos Point Six',
+        label: 'React front-end',
+        caption: 'Use of multiple libraries and bootstrap',
+        src: slide3
+    },
+    {
+        title: 'Cosmos Point Six',
+        label: 'Node/Express back-end',
+        caption: 'Mongoose module for DB queries',
+        src: slide4
+    },
+    {
+        title: 'Cosmos Point Six',
+        label: 'CosmosDB Mongo API',
+        caption: 'Configured on Azure free tier',
+        src: slide5
+    }
+]
--- src/components/reusables/Project.js
+++ src/components/reusables/Project.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+import '../../css/project.css'
 
 export const Project = (props) => {
     return (
-        <div>
-            
-        </div>
+        <Carousel className="projslides">
+            {props.slides.map((slide) => (
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slide.src}
+                        alt={slide.title}
+                    />
+                    <Carousel.Caption>
+                        <h3>{slide.label}</h3>
+                        <p>{slide.caption}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+            ))}
+        </Carousel>
     )
 }
